0a5a4fd924789b5ac12ee7869bbb0310a1553555
Sandeep Menon <menonsandu@gmail.com>
2024-01-05 15:32:54 +0530

    refence section completed

--- projects/ownership_tutorial/src/main.rs
+++ projects/ownership_tutorial/src/main.rs
@@ -46,8 +46,44 @@ fn main() {
     // So RUST has references. For using a value without transferring ownership
 
     let s1 = String::from("hello with ref");
-    let len = calculate_length_ref(&s1);
+    let len = calculate_length_ref(&s1);    // This is borrowing. The variable in the function does not own it.
     println!("The length of '{}' is {}.", s1, len);
+
+    let s = String::from("hello");
+    // change_ref(&s);  // This function attempts to modify the value of the modified reference. References are immutable by default.
+    
+    // To allow for mutable reference. Given example.
+    let mut s = String::from("hello");
+    change_mut_ref(&mut s);
+
+    // We can only have one mutable reference to a value.
+    let r1 = &mut s;
+    // let r2 = &mut s;
+    // println!("{}, {}", r1, r2);
+    println!("Mutable reference r1 of s: {}", r1);
+    // The above mechanism is important to avoid data races.
+    // We can create multiple mutable references but not simultaneously.
+
+    // Also cannot have a mutable reference while we have an immutable reference to the same value.
+    let r2 = &s;
+    let r3 = &mut s;    
+    // println!("{}, {}, and {}", r1, r2, r3); // No can do, if we try to access them in the same scope.
+
+    // However, the below example works. Because the references scope starts from where it was created to the last point where it is referenced.
+    // The compiler can undertand that.
+    let mut s = String::from("hello");
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+
+    // Rust does not allow Dangling references
+    // let reference_to_nothing = dangle();
+
+
 }
 
 fn takes_ownership(some_string: String) {
@@ -76,3 +112,18 @@ fn calculate_length(s: String) -> (String, usize) {
 fn calculate_length_ref(s: &String) -> usize {
     s.len()
 }
+
+// This does not compile as it attempts to change a reference, which is immutable.
+// fn change_ref(some_string: &String) {
+//     some_string.push_str(", world");
+// }
+
+fn change_mut_ref(some_string: &mut String) {
+    some_string.push_str(", world");
+
+}
+
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//     &s  // Returning a reference
+// }   // s goes out of scope and dropped. Now the reference does not point to anything. Hence Rust does not allow this.
