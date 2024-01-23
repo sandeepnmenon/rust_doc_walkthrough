f5520925111333271dce1dab7a77515e89df6bbd
Sandeep Menon <menonsandu@gmail.com>
2024-01-23 19:19:23 +0530

    rectangle project

---
+++ projects/rectangles/src/main.rs
@@ -0,0 +1,62 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    // Version 1
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels",
+        area(width1, height1)
+    );
+
+    // Version 2 with tuples
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels",
+        area_tuple(rect1)
+    );
+
+    // Version 3 with structs
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels",
+        area_struct(&rect1)
+    );
+
+    println!("Rect1 is {:?}", rect1);
+    println!("Rect1 is {:#?}", rect1); // Prettier printing
+
+    // Another way is to use the !dbg macro
+    // this takes ownership of the value and then returns it as opposed to println!
+    // Also this prints to stderr as opposed to stdout
+    // Also prints line number and file path
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
