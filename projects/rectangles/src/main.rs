52cac39d3d3f51be70bfff1761b8590957d90ca9
Sandeep Menon <menonsandu@gmail.com>
2024-01-24 12:47:25 +0530

    functions in structs

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -8,6 +8,21 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -59,6 +74,30 @@ fn main() {
         "The area of the rectangle is {} square pixels",
         rect1.area()
     );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width)
+    }
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    // Associated functions
+    let sq = Rectangle::square(3);
+    println!("Square created: {:?}", sq);
 }
 
 fn area(width: u32, height: u32) -> u32 {
