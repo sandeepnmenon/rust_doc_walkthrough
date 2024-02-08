79d2f3eabbec0b47bb047e77e8597ef0915b54c6
Sandeep Menon <menonsandu@gmail.com>
2024-02-08 13:14:23 -0500

    Add restaurant project and module structure

---
+++ projects/restaurant/src/lib.rs
@@ -0,0 +1,26 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn server_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+// module tree structure for this crate
+// crate
+//  └── front_of_house
+//      ├── hosting
+//      │   ├── add_to_waitlist
+//      │   └── seat_at_table
+//      └── serving
+//          ├── take_order
+//          ├── serve_order
+//          └── take_payment
