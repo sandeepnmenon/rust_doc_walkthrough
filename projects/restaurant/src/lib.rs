a83d483eba1186dc33e1507070a780cab2934b1d
Sandeep Menon <menonsandu@gmail.com>
2024-02-10 16:22:19 -0500

    moved module front_of_house to another file

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,18 +1,6 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn server_order() {}
-
-        fn take_payment() {}
-    }
-}
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     //Absolute path
