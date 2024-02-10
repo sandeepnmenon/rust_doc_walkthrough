2605191137516fb8781938fa2458af44e1d7a415
Sandeep Menon <menonsandu@gmail.com>
2024-02-10 15:59:15 -0500

    1. crate paths 2. struct and enum in modules

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -14,6 +14,14 @@ mod front_of_house {
     }
 }
 
+pub fn eat_at_restaurant() {
+    //Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    //Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
 // module tree structure for this crate
 // crate
 //  └── front_of_house
@@ -24,3 +32,52 @@ mod front_of_house {
 //          ├── take_order
 //          ├── serve_order
 //          └── take_payment
+
+// The module tree should be defined in src/lib.rs.
+//Then, any public items can be used in the binary crate by starting paths with the name of the package.
+//The binary crate becomes a user of the library crate just like a completely external crate would use the library crate:
+//it can only use the public API. This helps you design a good API; not only are you the author, you’re also a client!
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // If enum is public, all variants are public
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn eat_at_restaurant_2() {
+    let mut meal = back_of_house::Breakfast::summer(("Rye"));
+    meal.toast = String::from("wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // Below line wont compile as it is accessing a private attribute
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // If enum is public, all variants are public
+    let _order1 = back_of_house::Appetizer::Soup;
+    let _order2 = back_of_house::Appetizer::Salad;
+}
