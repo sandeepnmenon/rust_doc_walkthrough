d9bd8f1b2bf7f1b1a386dd4d9b432332069296e2
Sandeep Menon <menonsandu@gmail.com>
2024-02-12 19:16:30 -0500

    Refactor pig latin conversion logic to be robust to UTF-8 encoding

--- projects/collections_exercise/src/pig_latin.rs
+++ projects/collections_exercise/src/pig_latin.rs
@@ -2,11 +2,18 @@ pub fn to_pig_latin(s: &String) -> String {
     let mut pig_latin_s = String::new();
 
     for word in s.split_whitespace() {
-        if word.starts_with(&['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']) {
+        if word.is_empty() {
+            continue;
+        }
+
+        let first_char = word.chars().next().unwrap();
+
+        if ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].contains(&first_char) {
             pig_latin_s.push_str(&format!("{word}-hay "));
         } else {
-            let first_char = word.chars().next().unwrap_or(' ');
-            let exclude_first_char = &word[1..];
+            let mut word_chars = word.chars();
+            word_chars.next();
+            let exclude_first_char: String = word_chars.collect();
             pig_latin_s.push_str(&format!("{exclude_first_char}-{first_char}ay "));
         }
     }
