c08826aff9adffc962d533b225d8895a5bdef710
Sandeep Menon <menonsandu@gmail.com>
2024-02-29 23:56:57 -0500

    Add error handling and file operations

--- projects/error_handling/src/main.rs
+++ projects/error_handling/src/main.rs
@@ -1,5 +1,82 @@
-fn main() {
+use std::error::Error;
+use std::fs::{self, File};
+use std::io::{self, ErrorKind, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
     let v = vec![1, 2, 3];
 
-    v[99];
+    // v[99]; // will panic!
+
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", error),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+
+    // Same thing with unwrap_or_else
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    // unwrap and expect: Default behaviour to panic if error
+    // let greeting_file = File::open("hello_unwrap.txt").unwrap();
+    // Rustaceans prefer expect rather than unrwap and give more context to why the operation is expected to succeed
+    let greeting_file =
+        File::open("hello_unwrap.txt").expect("hello_unwrap.txt should be included in the project");
+
+    Ok(())
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+// Propagating errors is so common that Rust provides the ? operator to do the same
+// If value of result is Ok, it gets assigned to the expression
+// If Err, then function returns with Err
+fn read_username_from_file_1() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+// ? also supports chaining
+fn read_username_from_file_2() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+// even shorter
+fn read_username_from_file_3() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
