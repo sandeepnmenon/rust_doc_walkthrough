3472c9ce82769c7c53a9753e594375ae45ecd605
Sandeep Menon <menonsandu@gmail.com>
2024-03-06 17:29:37 -0500

    Refactor find_largest function to support different types

--- projects/generics/src/main.rs
+++ projects/generics/src/main.rs
@@ -1,4 +1,16 @@
-fn find_largest(list: &[i32]) -> &i32 {
+fn find_largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for value in &list[1..] {
+        if value > largest {
+            largest = value;
+        }
+    }
+
+    largest
+}
+
+fn find_largest_char(list: &[char]) -> &char {
     let mut largest = &list[0];
 
     for value in &list[1..] {
@@ -12,10 +24,14 @@ fn find_largest(list: &[i32]) -> &i32 {
 
 fn main() {
     let arr = vec![1, 2, 4, 8, 16, 32, 64, 128];
-    let largest = find_largest(&arr);
+    let largest = find_largest_i32(&arr);
     println!("The largest number is {}", largest);
 
     let arr = vec![1, 2, 4, 6, 8, 10, 12, 14];
-    let largest = find_largest(&arr);
+    let largest = find_largest_i32(&arr);
+    println!("The largest number is {}", largest);
+
+    let arr = vec!['a', 'c', 'z', 'b'];
+    let largest = find_largest_char(&arr);
     println!("The largest number is {}", largest);
 }
