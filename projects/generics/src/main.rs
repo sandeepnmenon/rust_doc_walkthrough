7738c47fe6ee75ebc7314b367906bcff4f5c7038
Sandeep Menon <menonsandu@gmail.com>
2024-03-06 17:33:27 -0500

    Add generic function to find largest element

--- projects/generics/src/main.rs
+++ projects/generics/src/main.rs
@@ -22,6 +22,18 @@ fn find_largest_char(list: &[char]) -> &char {
     largest
 }
 
+fn find_largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for value in &list[1..] {
+        if value > largest {
+            largest = value;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let arr = vec![1, 2, 4, 8, 16, 32, 64, 128];
     let largest = find_largest_i32(&arr);
@@ -33,5 +45,14 @@ fn main() {
 
     let arr = vec!['a', 'c', 'z', 'b'];
     let largest = find_largest_char(&arr);
+    println!("The largest number is {}", largest);
+
+    // With generic function
+    let arr = vec![1, 2, 4, 8, 16, 32, 64, 128];
+    let largest = find_largest(&arr);
+    println!("The largest number is {}", largest);
+
+    let arr = vec!['a', 'c', 'z', 'b'];
+    let largest = find_largest(&arr);
     println!("The largest number is {}", largest);
 }
