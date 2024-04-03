f4de17dcbf5516cd0c68e6f8c6536f33846e5c1f
Sandeep Menon <menonsandu@gmail.com>
2024-04-03 01:46:57 -0400

    Add aggregator module and implement Summary trait for NewsArticle, Tweet, and Journal

--- projects/traits/src/main.rs
+++ projects/traits/src/main.rs
@@ -1,3 +1,26 @@
+mod aggregator;
+use aggregator::{Journal, Summary, Tweet};
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+
+    // Default train
+    let journal = Journal {
+        title: String::from("Test journal"),
+        author: String::from("Me"),
+        content: String::from("No content"),
+    };
+
+    println!("New journal availabel!: {}", journal.summarize());
+
+    notify(&tweet);
 }
