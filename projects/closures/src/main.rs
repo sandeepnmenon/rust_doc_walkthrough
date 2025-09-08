d529f33262ef05f28438e451ae6a8099e73e144e
Sandeep Menon <menonsandu@gmail.com>
2025-09-08 15:32:29 -0700

    Enhance closure examples and add rectangle sorting functionality

--- projects/closures/src/main.rs
+++ projects/closures/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
@@ -31,6 +33,12 @@ impl Inventory {
     }
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let store = Inventory {
         shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
@@ -49,4 +57,47 @@ fn main() {
         "The user with preference {:?} gets {:?}",
         user_preference, giveaway
     );
+
+    let example_closure = |x| x;
+    let _s = example_closure(String::from("hello"));
+    // let n = example_closure(5);// Will not compile as the compile can only assign one type for closure params.
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure : {list:?}");
+
+    let only_borrows = || println!("From closure: {list:?}");
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}");
+
+    let mut list = vec![3, 2, 1];
+    println!("Before defining closure : {list:?}");
+    let mut borrows_mutably = || list.push(7);
+
+    // println!("Before calling closure: {list:?}"); // Does not compile as list is borrowed immutably here but it is also borrowed mutable by the closure.
+    borrows_mutably();
+    println!("After calling closure: {list:?}");
+
+    println!("Before calling closure for spawn thread: {list:?}");
+    thread::spawn(move || println!("From thread: {list:?}"))
+        .join()
+        .unwrap();
+    // println!("After calling closure for spawn thread: {list:?}");//Wont compile as list is borrowed/moved by the thread.
+
+
+    let mut list = [
+       Rectangle { width:10 , height:1 }, 
+       Rectangle { width:3 , height:5 },
+       Rectangle { width:7 , height:12 }, 
+    ];
+    list.sort_by_key(|x|x.width);
+    println!("{list:#?}");
+
+    let mut num_sort_operations = 0;
+    list.sort_by_key(|x| {
+        num_sort_operations+=1;
+        x.height
+    });
+    println!("{list:#?}, sorted in {num_sort_operations} operations");
+
 }
